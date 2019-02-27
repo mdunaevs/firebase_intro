92db69743c0d8da1cf23361b0bb6c16db17ab06e
mdunaevs <44279139+mdunaevs@users.noreply.github.com>
2019-02-26 21:19:40 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+# Video link to how to create message board: https://www.youtube.com/watch?v=Di607bTqhPc
+
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList} from 'react-native';
 import {Constants} from 'expo';
